119f4a3b875fe9ab70c2b33545bd508a555dd610
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-03-18 23:42:59 +0100

    Update Social-media.js

---
+++ src/Components/So/Social-media.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Socialmedia() {
+  return (
+    <div>
+        <div>
+            <ul>
+                yassin
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                yassin
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Socialmedia
